268b5326ac974ee2eecfc6ec4c992102b1f3e853
Yunhui Bae <yubae@student.42seoul.kr>
2022-07-13 17:25:11 +0900

    asdf

--- 630/srcs/requirements/wordpress/tools/configure.sh
+++ 630/srcs/requirements/wordpress/tools/configure.sh
@@ -5,7 +5,9 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
 
 	cp /tmp/wp-config.php /var/www/html/wp-config.php
 	sleep 5;
-	mysqladmin -h $MARIADB_HOST -u $WP_DATABASE_NAME --password=$WP_DATABASE_PWD --wait=60 ping 
+	mkdir -p /var/www/html
+	wget https://raw.githubusercontetn.com
+	mysqladmin -h $MARIADB_HOST -u $WP_DATABASE_NAME --password=$WP_DATABASE_PWD --wait=60 ping
     wp core download --allow-root
     #wp core config --dbname=$WP_DATABASE_NAME --dbuser=$WP_DATABASE_USR --dbpass=$WP_DATABASE_PWD --dbhost=$MRIADB_HOST --allow-root
     wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
